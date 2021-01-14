074b8758ac04dfccb477ef8b4ccc347fc1c29223
Deokyun-Jang <luca911014@gmail.com>
2021-01-15 02:38:52 +0900

    add ml parsing

--- rosbag_to_bin.py
+++ rosbag_to_bin.py
@@ -31,24 +31,35 @@ if __name__=='__main__':
         print(save_path)
         for lidar_topic, lidar_msg, lidar_t in bag.read_messages(topics=[args.lidar_topic]):
             
+            test_list_pc = [] # test
+            
             # lidar msg to numpy array(n,4)
             list_pc = []
-            for data in pc2.read_points(lidar_msg, skip_nans=True):
+            for data in pc2.read_points(lidar_msg, skip_nans=False):
                 #print( float_to_rgb(data[3]))
                 #print('------------------')
                 if args.is_ml == True:
                     rgb = float_to_rgb(data[3])
-                    list_pc.append( [data[0], data[1], data[2]]+rgb )
+                    xyzrgb = [data[0], data[1], data[2]]+rgb 
+                    #print(xyzrgb)
+                    list_pc.append( xyzrgb )
+                    
+                    #test
+                    test_list_pc.append([data[0], data[1], data[2], data[3]])
+
                 else:
                     list_pc.append([data[0], data[1], data[2], data[3]])
             np_pc = np.array(list_pc, dtype=np.float32)
+            print(np_pc.shape)
+            
+            # test
+
 
             # save numpy array to bin or pcd
             filename = str(lidar_t)
             print(filename)
             
             np_pc_bin = copy.deepcopy(np_pc)
-            print(np_pc_bin.shape)
             bin_path_ = save_path+'/bin'
             Path(bin_path_).mkdir(parents=True, exist_ok=True)
             bin_path = bin_path_ + "/" + filename + ".bin"
@@ -60,10 +71,9 @@ if __name__=='__main__':
             pcd_path = pcd_path_+"/"+filename+".pcd"
             
             if args.is_ml == True:
-                #pc_rgb = pcl.PointCloud_PointXYZRGB()
-                #pc_rgb.from_list(list_pc)
-                #pcl.save(pc_rgb, pcd_path)
-                pass
+                pc_rgb = pcl.PointCloud_PointXYZRGB()
+                pc_rgb.from_list(test_list_pc)
+                pcl.save(pc_rgb, pcd_path)
             else:
                 pc_intensity = pcl.PointCloud_PointXYZI()
                 pc_intensity.from_array(np_pc_pcd)
